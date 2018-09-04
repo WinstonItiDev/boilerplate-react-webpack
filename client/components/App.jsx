283c3add2459058d49d8196ca8e9dc2ad93dd85e
Nat <nat_sirisamphan@hotmail.com>
2018-09-05 11:53:14 +1200

    proverb api working

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,9 +1,41 @@
 import React from 'react'
+import request from 'superagent'
 
-const App = () => {
-  return (
-    <h1>React development has begun!</h1>
-  )
+const apiEndPoint = 'https://eda-te-reo.herokuapp.com/api/proverbs'
+
+class App extends React.Component  {
+  constructor (props) {
+    super(props)
+  
+    this.state = {
+      proverb: []
+    }
+    this.getProverb = this.getProverb.bind(this)
+  }
+
+  getProverb() {
+    return request.get(apiEndPoint)
+    .then(res => {
+      console.log(res.text)
+      this.setState({
+        proverb: res.body
+      })
+    })
+  }
+
+  render () {
+    return (
+      <div>
+        <h1>Te Reo translations:</h1>
+        <button onClick={this.getProverb}>Get Proverb</button>
+        <h3>Te Reo:</h3>
+        <p>{this.state.proverb.source}</p>
+        <h3>English:</h3>
+        <p>{this.state.proverb.translation}</p>
+      </div>
+    )
+  }
+
 }
 
 export default App
