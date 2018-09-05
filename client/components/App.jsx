ae775ff3d4f05318cc47ae49725a6cc1ef275944
Nat <nat_sirisamphan@hotmail.com>
2018-09-05 15:28:21 +1200

    added meme chuck norris gen

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -3,6 +3,7 @@ import request from 'superagent'
 
 const apiEndPoint = 'https://eda-te-reo.herokuapp.com/api/proverbs'
 const seagalImg = 'https://www.stevensegallery.com/'
+const memeGen = 'https://geek-jokes.sameerkumar.website/api'
 
 class App extends React.Component  {
   constructor (props) {
@@ -10,10 +11,12 @@ class App extends React.Component  {
   
     this.state = {
       proverb: [],
-      img: []
+      img: [],
+      meme: []
     }
     this.getProverb = this.getProverb.bind(this)
     this.getSeagal = this.getSeagal.bind(this)
+    this.getMeme = this.getMeme.bind(this)
   }
 
   getProverb() {
@@ -34,12 +37,30 @@ class App extends React.Component  {
       })
   }
 
+  getMeme() {
+
+    return request.get(memeGen)
+      .then(res => {
+        console.log(res)
+      })
+
+    // return request.get(memeGen).set('X-Mashape-Key', 'Ltov54pZPAmshLuD5uyb4HjSAeqfp1EGrXXjsnsNpXYKg5N5Wt')
+    //   .then(res => {
+    //     console.log(res)
+    //   })
+    //   .catch(err => {
+    //     console.log(err)
+    //   })
+  }
+
   render () {
     return (
+      
       <div>
         <h1>Te Reo translations:</h1>
         <button onClick={this.getProverb}>Get Proverb</button>
         <button onClick={this.getSeagal}>Get Seagal</button>
+        <button onClick={this.getMeme}>Generate Meme</button>
         <h3>Te Reo:</h3>
         <p>{this.state.proverb.source}</p>
         <h3>English:</h3>
