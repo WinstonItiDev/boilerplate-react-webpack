c74fd3e750e9542788fe04770f22dcacbe4fae87
Nat <nat_sirisamphan@hotmail.com>
2018-09-05 12:30:45 +1200

    added seagal pics

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,25 +2,33 @@ import React from 'react'
 import request from 'superagent'
 
 const apiEndPoint = 'https://eda-te-reo.herokuapp.com/api/proverbs'
+const seagalImg = 'https://www.stevensegallery.com/'
 
 class App extends React.Component  {
   constructor (props) {
     super(props)
   
     this.state = {
-      proverb: []
+      proverb: [],
+      img: []
     }
     this.getProverb = this.getProverb.bind(this)
+    this.getSeagal = this.getSeagal.bind(this)
   }
 
   getProverb() {
     return request.get(apiEndPoint)
     .then(res => {
-      console.log(res.text)
       this.setState({
         proverb: res.body
       })
     })
+  }
+
+  getSeagal() {
+      this.setState({
+        img: seagalImg + '/' + '600' + '/' + '800'
+      })
   }
 
   render () {
@@ -28,10 +36,12 @@ class App extends React.Component  {
       <div>
         <h1>Te Reo translations:</h1>
         <button onClick={this.getProverb}>Get Proverb</button>
+        <button onClick={this.getSeagal}>Get Seagal</button>
         <h3>Te Reo:</h3>
         <p>{this.state.proverb.source}</p>
         <h3>English:</h3>
         <p>{this.state.proverb.translation}</p>
+        <img src={this.state.img}/>
       </div>
     )
   }
